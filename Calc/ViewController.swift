6f7d89b61a57cea2546f9099197e3efca53e9d5d
Admin <admin@Mac.local>
2022-04-05 07:02:39 +0300

    First message

---
+++ Calc/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Calc
+//
+//  Created by Admin on 30.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var buttonAC: UIButton!
+    @IBOutlet weak var buttonZero: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
